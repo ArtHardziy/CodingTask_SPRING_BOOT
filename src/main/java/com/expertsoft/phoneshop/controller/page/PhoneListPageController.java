b4aece1fffe586d9aff82686ef36c19833693414
MRw-Overlord <artem.gordievich.02@mail.ru>
2023-11-16 10:27:09 +0300

    some fixes

--- src/main/java/com/expertsoft/phoneshop/controller/page/PhoneListPageController.java
+++ src/main/java/com/expertsoft/phoneshop/controller/page/PhoneListPageController.java
@@ -1,16 +1,15 @@
 package com.expertsoft.phoneshop.controller.page;
 
-import com.expertsoft.phoneshop.persistence.model.Phone;
-import com.expertsoft.phoneshop.persistence.model.dto.PlpDto;
 import com.expertsoft.phoneshop.persistence.model.dto.SearchFormModel;
 import com.expertsoft.phoneshop.service.PhoneService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import static com.expertsoft.phoneshop.PhoneShopConstants.PHONES_PATH;
 import static com.expertsoft.phoneshop.service.AppUtil.processPageableRequest;
@@ -26,31 +25,48 @@ public class PhoneListPageController {
 
     private PhoneService phoneService;
 
-    @GetMapping
-    public String getPhoneList(@RequestParam(defaultValue = "1") String pageNum,
-                               @RequestParam(required = false, defaultValue = "10") String pageSize,
-                               @RequestParam(defaultValue = "id") String sortBy,
-                               @RequestParam(defaultValue = "ASC") String sortOrder,
-                               @ModelAttribute("searchFormModel") SearchFormModel searchFormModel,
-                               BindingResult bindingResult,
-                               Pageable pageable,
-                               Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("error", "Check entered values");
-            return PHONE_LIST_PAGE;
-        }
-        Pageable pageRequest = processPageableRequest(pageNum, pageSize, sortBy, sortOrder, pageable);
-        PlpDto<Phone> phonesPlpDto = phoneService.getPhonesPlp(pageRequest, searchFormModel);
-        model.addAttribute(PHONES_PAGE, phonesPlpDto);
-        model.addAttribute("searchFormModel", searchFormModel);
-        return PHONE_LIST_PAGE;
+    @PostMapping
+    public ModelAndView submitSearch(Model model,
+            @ModelAttribute("searchFormModel") SearchFormModel searchFormModel,
+                                      BindingResult bindingResult) {
+        var pagedProductListPage = getPagedProductListPage(model,
+                1,
+                7,
+                "id",
+                "asc",
+                searchFormModel,
+                bindingResult);
+
+        return pagedProductListPage;
     }
 
-    @PostMapping
-    public String submitSearch(Model model,
-                               @ModelAttribute("searchFormModel") SearchFormModel searchFormModel,
-                               BindingResult bindingResult) {
-        getPhoneList("1", "10", "id", "asc", searchFormModel, bindingResult, null, model);
-        return PHONE_LIST_PAGE;
+    @GetMapping
+    public ModelAndView getPagedProductListPage(
+            Model model,
+            @RequestParam(defaultValue = "1") Integer pageNum,
+            @RequestParam(required = false, defaultValue = "7") Integer pageSize,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortOrder,
+            SearchFormModel searchFormModel,
+            BindingResult bindingResult
+    ) {
+        var modelAndView = new ModelAndView(PHONE_LIST_PAGE);
+        if (bindingResult.hasErrors()) {
+            modelAndView.addObject("searchFormModelError", true);
+            modelAndView.setStatus(HttpStatus.PERMANENT_REDIRECT);
+            return modelAndView;
+        }
+        try {
+            modelAndView.addObject(PHONES_PAGE,
+                    phoneService.getPhonePlp(searchFormModel,
+                            processPageableRequest(pageNum, pageSize, sortBy, sortOrder))
+            );
+        } catch (Exception ex) {
+            modelAndView.addObject("error", "Error during forming PLP. Redirected to home");
+            log.error("Error during forming PLP", ex);
+            modelAndView.setStatus(HttpStatus.PERMANENT_REDIRECT);
+            return modelAndView;
+        }
+        return modelAndView;
     }
 }
---
+++ src/main/java/com/expertsoft/phoneshop/persistence/repository/PhoneRepository.java
@@ -0,0 +1,12 @@
+package com.expertsoft.phoneshop.persistence.repository;
+
+import com.expertsoft.phoneshop.persistence.model.Phone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PhoneRepository extends JpaRepository<Phone, Long> {
+
+    List<Phone> findAllByModelContainingIgnoreCaseOrBrandContainingIgnoreCase(String model, String brand);
+
+}
